c93ef4bee329003f379c8ccc805847fa69e3bbc6
natthacha <natthacha.a@gmail.com>
2014-10-19 23:49:10 -0400

    hero background

--- crhouse-netbeans/public_html/js/main.js
+++ crhouse-netbeans/public_html/js/main.js
@@ -1,64 +1,35 @@
 
 var main = function(){
-                var inverse =true;
-                var open = false;
-
+    var bgtransparent=true;
     //toggle navbar color when scrolldown
     $(window).scroll(function(){
-       /* if ($(this).scrollTop() > 50) {
-            $('#navbar').removeClass('navbar-inverse');
-            $('#navbar').addClass('navbar-default');
-        } else {
-            $('#navbar').removeClass('navbar-default');
-            $('#navbar').addClass('navbar-inverse');
-        }*/
-
-        if ($(this).scrollTop() > 50) { //scroll down pass
-            if(inverse&!open){ //if not open, change from transparent to default
-            $('#navbarinverse').animate({'opacity':0});
-            $('#navbardefault').animate({'opacity':1},300);
-            inverse=false;
+        if ($(this).scrollTop() < 60) {
+            if(!bgtransparent) {
+                $('#mainnav').animate({"background-color":"transparent"});
+                bgtransparent=true;
             }
-        } else {
-            if(!inverse&!open ){
-            $('#navbardefault').animate({'opacity':0});
-            $('#navbarinverse').animate({'opacity':1},300);
-            inverse=true;
-            }
-        }
-    });
-    
-        //Toggle negative/default navbar color when clicked
-    //TODO: change toggle speed
-    $('.navbar-toggle').click(function(){
-        
-        //toggle by change opacity
-        if($(window).scrollTop() > 50 ){
-        }else{
-            if(inverse){
-                $('#navbarinverse').animate({'opacity':0});
-                $('#navbardefault').animate({'opacity':1},100);
-                inverse=false;
-            }else{
-                $('#navbardefault').animate({'opacity':0});
-                $('#navbarinverse').animate({'opacity':1},300);
-                inverse=true;
-            }
-        }
-        open=!open;
-        /*if($(window).scrollTop() > 50 ) {
             
         } else {
-            if($('#navbar').hasClass('navbar-default')){
-                
-                setTimeout( function(){
-                        $('#navbar').stop(true,true).toggleClass('navbar-inverse navbar-default');
-                }, 500);
-            } else {
-                $('#navbar').toggleClass('navbar-inverse navbar-default',500);
+            if(bgtransparent) {
+                $('#mainnav').animate({"background-color":"white"});
+                bgtransparent=false;
             }
-            //$('#navbarinverse').
-        }*/
+        };
+     
+    });
+    
+    //toggle color when transparent navbar collapse
+    $('.navbar-toggle').click(function(){
+        
+        if($(window).scrollTop() < 60 ){
+            if(bgtransparent){
+                $('#mainnav').animate({"background-color":"white"});
+            }else{
+                $('#mainnav').animate({"background-color":"transparent"});
+            }
+        }
+        bgtransparent=!bgtransparent;
+       
     });
     
 };
